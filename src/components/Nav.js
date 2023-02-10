8b90ccefdfad2e0063cfc078b7ad9e5eb169bc45
stetsonharding <stetson2001@msn.com>
2023-02-10 12:26:25 -0800

    navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+//IMporting The Kushery Logo
+import Logo from "../assets/Logo.png";
+//Importing Nav css file
+import "../css/Nav.css";
+
+function Nav() {
+  return (
+    <>
+      <Navbar variant="dark" style={{ backgroundColor: "#ED820E" }}>
+        <Container fluid className="m-2">
+          <Navbar.Brand>
+            <img className="logo" alt="" src={Logo} width="45" height="35" />{" "}
+          </Navbar.Brand>
+          <Navbar.Brand
+            className="nav-brand"
+            style={{ color: "green", fontSize: "22px" }}
+          >
+            The Kushery Apparel
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Nav;
